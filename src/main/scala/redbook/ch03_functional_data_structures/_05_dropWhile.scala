43c59199587b74d65a6941626bacb381cecc90ac
mahendra.pk <mahendra.pk@gmail.com>
2018-03-16 07:51:48 +0530

    Exercise 3.5 : curried dropWhile does smart Type inference

--- src/main/scala/redbook/ch03_functional_data_structures/_05_dropWhile.scala
+++ src/main/scala/redbook/ch03_functional_data_structures/_05_dropWhile.scala
@@ -1,8 +1,20 @@
 package redbook.ch03_functional_data_structures
 
+/*
+  Exercise 3.5 description:
+  Implement dropWhile, which removes elements from the List prefix as long as they match a predicate.
+ */
+
 object _05_dropWhile {
-  def dropWhile[T](l: MyList[T], f: T => Boolean): MyList[T] = l match {
+  def dropWhile[A](l: MyList[A], f: A => Boolean): MyList[A] = l match {
     case Cons(h, t) if f(h) ⇒ dropWhile(t, f)
     case _                  ⇒ l
   }
+
+  /*
+    Additional notes:
+    Unfortunately at the call site scala can't infer the type of T passed in the predicate parameter. Currying solves it.
+   */
+
+  def dropWhile2[A](l: MyList[A])(f: A ⇒ Boolean): MyList[A] = dropWhile(l, f)
 }
--- src/test/scala/redbook/ch03_functional_data_structures/_05_dropWhileTest.scala
+++ src/test/scala/redbook/ch03_functional_data_structures/_05_dropWhileTest.scala
@@ -6,10 +6,15 @@ class _05_dropWhileTest extends FunSuite with Matchers {
 
   import redbook.ch03_functional_data_structures._05_dropWhile._
 
-  test("testDropWhile") {
+  test("dropWhile") {
     dropWhile(MyList(1, 2, 3, 4), (x: Int) => x < 3) shouldEqual MyList(3, 4)
     dropWhile(MyList(1, 2, 3, 4), (x: Int) => x != 5) shouldEqual Nil
     dropWhile(Nil, (x: Int) => x > 10) shouldEqual Nil
+  }
+
+  test("curried dropWhile makes smart Type inference") {
+    dropWhile2(MyList(1, 2, 3, 4))(x => x < 3) shouldEqual MyList(3, 4)
+    dropWhile2(MyList(1, 2, 3, 4))(x => x != 5) shouldEqual Nil
   }
 
 }
