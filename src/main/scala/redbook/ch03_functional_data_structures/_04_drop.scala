1404eabe2884ea1e806f495ff14ad31fa38e59b6
Mahendra <Trackless.Vale@gmail.com>
2018-03-20 02:15:33 +0530

    Exercise description for 3.4, 3.14

--- src/main/scala/redbook/ch03_functional_data_structures/_04_drop.scala
+++ src/main/scala/redbook/ch03_functional_data_structures/_04_drop.scala
@@ -1,5 +1,12 @@
 package redbook.ch03_functional_data_structures
 
+/*
+  Exercise 3.4 description:
+  Generalize tail to the function drop, which removes the first n elements from a list.
+  Note that this function takes time proportional only to the number of elements being
+  dropped—we don’t need to make a copy of the entire List.
+ */
+
 object _04_drop {
 
   def drop[T](l: MyList[T], n: Int): MyList[T] = {
--- src/main/scala/redbook/ch03_functional_data_structures/_14_append.scala
+++ src/main/scala/redbook/ch03_functional_data_structures/_14_append.scala
@@ -1,5 +1,11 @@
 package redbook.ch03_functional_data_structures
 
+/*
+  Exercise 3.14 description:
+
+  Implement append in terms of either foldLeft or foldRight.
+ */
+
 object _14_append {
   import redbook.ch03_functional_data_structures.MyList._
   import redbook.ch03_functional_data_structures._10_foldLeft._
