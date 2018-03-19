45103714c8e3c19dfeb24324f8a46743481ac0c1
Mahendra <Trackless.Vale@gmail.com>
2018-03-20 02:36:22 +0530

    Added descrption

--- src/main/scala/redbook/ch03_functional_data_structures/_15_ListOfListsConcat.scala
+++ src/main/scala/redbook/ch03_functional_data_structures/_15_ListOfListsConcat.scala
@@ -1,5 +1,12 @@
 package redbook.ch03_functional_data_structures
 
+/*
+  Exercise 3.15 description:
+  Hard: Write a function that concatenates a list of lists into a single list. Its runtime
+  should be linear in the total length of all lists. Try to use functions we have already
+  defined.
+ */
+
 import redbook.ch03_functional_data_structures.MyList._
 import redbook.ch03_functional_data_structures._14_append._
 
