8f289faeee53579d7b7505688046a55174b41f2e
Mahendra <Trackless.Vale@gmail.com>
2018-03-20 02:10:01 +0530

    3.14; append using foldLeft and foldRight

---
+++ src/test/scala/redbook/ch03_functional_data_structures/_14_appendTest.scala
@@ -0,0 +1,12 @@
+package redbook.ch03_functional_data_structures
+
+import org.scalatest.{FunSuite, Matchers}
+
+class _14_appendTest extends FunSuite with Matchers {
+  import redbook.ch03_functional_data_structures._14_append._
+
+  test("append") {
+    appendUsingFoldRight(MyList(1, 2, 3, 4), MyList(5, 6, 7, 8)) shouldEqual MyList(1, 2, 3, 4, 5, 6, 7, 8)
+    appendUisngFoldLeft(MyList(1, 2, 3, 4), MyList(5, 6, 7, 8)) shouldEqual MyList(1, 2, 3, 4, 5, 6, 7, 8)
+  }
+}
