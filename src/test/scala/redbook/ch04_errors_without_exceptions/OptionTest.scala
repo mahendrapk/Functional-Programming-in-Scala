924688ca12c0acb8c90758039f442c668b9cb1ba
Mahendra <Trackless.Vale@gmail.com>
2018-03-31 00:37:14 +0530

    4.1; added tests

---
+++ src/test/scala/redbook/ch04_errors_without_exceptions/OptionTest.scala
@@ -0,0 +1,46 @@
+package redbook.ch04_errors_without_exceptions
+
+import org.scalatest.{FunSuite, Matchers}
+
+class OptionTest extends FunSuite with Matchers {
+
+  private val intOpt    = Some(100)
+  private val stringOpt = Some("abcd")
+  private val noneOpt   = None
+
+  test("testFilter1") {
+    stringOpt.filter1(!_.isEmpty) shouldEqual stringOpt
+    noneOpt.filter1(_ != 100) shouldEqual None
+  }
+
+  test("testFilter") {
+    stringOpt.filter1(!_.isEmpty) shouldEqual stringOpt
+    noneOpt.filter1(_ != 100) shouldEqual None
+  }
+
+  test("testOrElse") {
+    intOpt.orElse(Some(200)) shouldEqual intOpt
+    noneOpt.orElse(stringOpt) shouldEqual stringOpt
+  }
+
+  test("testOrElse1") {
+    intOpt.orElse1(Some(200)) shouldEqual intOpt
+    noneOpt.orElse1(stringOpt) shouldEqual stringOpt
+  }
+
+  test("testFlatMap") {
+    intOpt.flatMap(x ⇒ Some(x * x)) shouldEqual intOpt.map(x ⇒ x * x)
+    noneOpt.flatMap(x ⇒ Some(x)) shouldEqual None
+  }
+
+  test("testGetOrElse") {
+    intOpt.getOrElse(200) shouldEqual 100
+    noneOpt.getOrElse("I was none") shouldEqual "I was none"
+  }
+
+  test("testMap") {
+    intOpt.map(x ⇒ x * x) shouldEqual Some(10000)
+    noneOpt.map(_.toString) shouldEqual None
+  }
+
+}
